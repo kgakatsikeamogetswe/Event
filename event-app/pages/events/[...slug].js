0b283449700112f4d0a05f537fe07e54f9505765
kgakatsikeamogetswe <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2023-09-05 16:11:17 +0200

    components

---
+++ event-app/pages/events/[...slug].js
@@ -0,0 +1,3 @@
+export default function FilteredEventsPage(){
+    return <div><h1>Filtered Events Page</h1></div>
+}
---
+++ event-app/pages/events/index.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+import { getAllEvents } from "../../dummy-data";
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+
+
+export default function AllEventsPage() {
+  const events = getAllEvents();
+
+  return (
+    <Fragment>
+      <EventsSearch />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
