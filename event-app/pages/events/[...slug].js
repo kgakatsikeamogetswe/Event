2a079a399196be9148437c2caf12fc1bb1d10f3b
kgakatsikeamogetswe <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2023-09-05 22:54:12 +0200

    slug functionality

--- event-app/pages/events/[...slug].js
+++ event-app/pages/events/[...slug].js
@@ -1,44 +1,70 @@
-import { useRouter } from 'next/router'
-import { getFilteredEvents } from '../../dummy-data'
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
 
 export default function FilteredEventsPage() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const filterData = router.query.slug;
+  const filterData = router.query.slug;
 
-    if (!filterData) {
-        return <p className="center">Loading.....</p>;
-    }
+  if (!filterData) {
+    return <p className="center">Loading.....</p>;
+  }
 
-    const filteredYear = filterData[0];
-    const filteredMonth = filterData[1];
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
 
-    const numYear = +filteredYear;
-    const numMonth = +filteredMonth
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
 
-    if (isNaN(numYear) || 
-    isNaN(numMonth) || 
-    numYear > 2030 || 
-    numYear < 2021 || 
-    numMonth < 1 || 
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
     numMonth > 12
-    ) {
-        return <p>Invalid filter. Please adjust your values!</p>
-    }
+  ) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className='center'>
+          <button link='/events'>Show All Events</button>
+        </div>
+      </Fragment>
+    );
+  }
 
-    const filteredEvents = getFilteredEvents({
-        year: numYear,
-        month: numMonth,
-    });
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
 
-    if (!filteredEvents || filteredEvents.length === 0){
-        return <p>No events found for the chosen filter!</p>
-    }
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+        <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
 
+  const date = new Date(numYear, numMonth - 1);
   return (
-    <div>
-      <h1>Filtered Events Page</h1>
-    </div>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
   );
 }
--- event-app/pages/events/[eventId].js
+++ event-app/pages/events/[eventId].js
@@ -5,6 +5,7 @@ import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
 export default function EventDetailPage() {
   const router = useRouter();
@@ -13,19 +14,20 @@ export default function EventDetailPage() {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No event found!</p>;
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
   }
 
   return (
     <Fragment>
       <EventSummary title={event.title} />
-      <EventLogistics 
-      date={event.date}
-      address={event.location} 
-      image={event.image}
-     
-      
-      
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
       />
       <EventContent>
         <p>{event.description}</p>
