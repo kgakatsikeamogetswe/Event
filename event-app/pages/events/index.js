0291e0eb2412ea3c07aa4392f357e0fadd95baad
kgakatsikeamogetswe <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2023-09-05 22:08:57 +0200

    search-event functionality

--- event-app/pages/events/index.js
+++ event-app/pages/events/index.js
@@ -1,4 +1,6 @@
 import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
@@ -6,10 +8,17 @@ import EventsSearch from '../../components/events/events-search';
 
 export default function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month){
+    const fullPath = `/events/${year}/ ${month}`;
+    
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler}/>
       <EventList items={events} />
     </Fragment>
   );
